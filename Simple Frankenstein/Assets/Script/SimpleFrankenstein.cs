8d40537790f3262bd8ffc65fc8355c82862a1d4a
Prima20 <primakurnia71@gmail.com>
2018-10-07 21:46:16 +0700

    Init commit

---
+++ Simple Frankenstein/Assets/Script/SimpleFrankenstein.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFrankenstein : MonoBehaviour {
+    //Apply this script to every component like face, hair, body etc
+    //Every component must have a SpriteRenderer component
+
+    //Component to build;
+    public Sprite[] component;
+
+    //Variable for gameobject SpriteRenderer component
+    private SpriteRenderer spriteComponent;
+
+	// Use this for initialization
+	void Start () {
+        //Apply component to SpriteRenderer
+        spriteComponent = GetComponent<SpriteRenderer>();
+        spriteComponent.sprite = component[Random.Range(0, component.Length)];
+	}
+
+}
